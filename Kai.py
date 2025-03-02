3c204469945ec33c1a60f8c2289e7293b05d5af2
gitoutkai <kaihehau@hawaii.edu>
2025-03-01 18:09:47 -1000

    Update Kai.py

--- Kai.py
+++ Kai.py
@@ -1,1 +1,8 @@
-print("hello, world")
+'''hey this is kaihehau "kai" goo. this file is being made on 2/20/25. this is actually my second coding project because I did variables before this for some reason. The most recent edit is 3/1/25 and Im really just here to write hello world and then profit for some reason'''
+
+
+print("Hello World!")
+
+print("Profit????")
+
+
